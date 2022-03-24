f0d248746140c3d328d3a98cfe8a7b76f161074e
piagja <piagja@gmail.com>
2022-03-24 19:50:53 -0300

    teste do CD

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send(`Hello World from port: ${PORT}`))
