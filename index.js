3f07bff0b96757a317525f9a12f9c4d7f6f8085e
piagja <piagja@gmail.com>
2022-03-24 18:52:15 -0300

    fix: port variable

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send(`Hello World from port: ${PORT}`))
-app.listen(port, () => console.log(`Example app listening on port ${PORT}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
